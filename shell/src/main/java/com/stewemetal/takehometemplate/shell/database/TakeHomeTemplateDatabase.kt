7da832bae32de58d05e2e6bb161016dc6b7443f8
István Juhos <stewemetal@gmail.com>
2023-08-01 23:50:42 +0200

    Add ExampleDatabaseEntity to TakeHomeTemplateDatabase

--- shell/src/main/java/com/stewemetal/takehometemplate/shell/database/TakeHomeTemplateDatabase.kt
+++ shell/src/main/java/com/stewemetal/takehometemplate/shell/database/TakeHomeTemplateDatabase.kt
@@ -2,9 +2,12 @@ package com.stewemetal.takehometemplate.shell.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.stewemetal.takehometemplate.shell.database.entity.ExampleDatabaseEntity
 
 @Database(
-    entities = [],
+    entities = [
+        ExampleDatabaseEntity::class,
+    ],
     version = 1,
 )
 abstract class TakeHomeTemplateDatabase : RoomDatabase() {
