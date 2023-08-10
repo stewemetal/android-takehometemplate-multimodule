7b754858ff43bc1699ff031d801422619406b888
István Juhos <stewemetal@gmail.com>
2023-08-11 00:12:47 +0200

    Fix some detekt warnings

--- shell/src/main/java/com/stewemetal/takehometemplate/shell/domain/usecase/GetItemUseCase.kt
+++ shell/src/main/java/com/stewemetal/takehometemplate/shell/domain/usecase/GetItemUseCase.kt
@@ -1,9 +1,9 @@
 package com.stewemetal.takehometemplate.shell.domain.usecase
 
 import com.stewemetal.takehometemplate.shell.architecture.DispatcherProvider
-import com.stewemetal.takehometemplate.shell.repository.ItemsRepository
 import com.stewemetal.takehometemplate.shell.domain.model.Item
 import com.stewemetal.takehometemplate.shell.domain.model.ItemId
+import com.stewemetal.takehometemplate.shell.repository.ItemsRepository
 import kotlinx.coroutines.withContext
 import org.koin.core.annotation.Singleton
 
--- shell/src/main/java/com/stewemetal/takehometemplate/shell/domain/usecase/GetItemsUseCase.kt
+++ shell/src/main/java/com/stewemetal/takehometemplate/shell/domain/usecase/GetItemsUseCase.kt
@@ -1,8 +1,8 @@
 package com.stewemetal.takehometemplate.shell.domain.usecase
 
 import com.stewemetal.takehometemplate.shell.architecture.DispatcherProvider
-import com.stewemetal.takehometemplate.shell.repository.ItemsRepository
 import com.stewemetal.takehometemplate.shell.domain.model.Item
+import com.stewemetal.takehometemplate.shell.repository.ItemsRepository
 import kotlinx.coroutines.withContext
 import org.koin.core.annotation.Singleton
 
