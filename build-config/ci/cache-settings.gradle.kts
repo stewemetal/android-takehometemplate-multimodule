7ffc2f873dca317cc28adc1fafa23c9bc92c361d
István Juhos <stewemetal@gmail.com>
2023-08-11 20:29:02 +0200

    Enable gradle build and config cache

---
+++ build-config/ci/cache-settings.gradle.kts
@@ -0,0 +1,8 @@
+beforeSettings {
+    caches {
+        releasedWrappers.setRemoveUnusedEntriesAfterDays(30)
+        snapshotWrappers.setRemoveUnusedEntriesAfterDays(7)
+        downloadedResources.setRemoveUnusedEntriesAfterDays(30)
+        createdResources.setRemoveUnusedEntriesAfterDays(3)
+    }
+}
