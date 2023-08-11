ccdc004777733e5d971e41d3bc9c068282e9341f
István Juhos <stewemetal@gmail.com>
2023-08-11 20:29:14 +0200

    Remove example code from MainActivity

--- app/src/main/java/com/stewemetal/takehometemplate/MainActivity.kt
+++ app/src/main/java/com/stewemetal/takehometemplate/MainActivity.kt
@@ -3,10 +3,6 @@ package com.stewemetal.takehometemplate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.stewemetal.takehometemplate.design.theme.TakeHomeTemplateTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,31 +10,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TakeHomeTemplateTheme {
-                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    Greeting("Android")
-//                }
                 TakeHomeTemplateApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TakeHomeTemplateTheme {
-        Greeting("Android")
     }
 }
